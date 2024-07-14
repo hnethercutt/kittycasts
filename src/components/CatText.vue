6213c0f7075fb7545a6928eb869176f221bb7d72
Hannah <144931749+conranh@users.noreply.github.com>
2024-07-13 20:28:20 -0400

    Added text box component with typewriter animation

---
+++ src/components/CatText.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+    const txt = "Testing testing";
+    let start = 0;
+
+    // Creates the typewrite effect in the text box when the page loads
+    const catText = setInterval(function() {
+        document.getElementById("textBox").innerHTML += txt.charAt(start);
+            if(++start == txt.length) {
+                clearInterval(catText);
+                $("#textBox").trigger($.Event("keypress", {
+                    keyCode: 13 
+                }));
+            }
+    }, 100);
+})
+</script>
+
+<template>
+    <div id="textBox" class="text-box"></div>
+</template>
+
+<style>
+.text-box {
+        background-color: rgba(0, 0, 0, 0.8);
+        border: 3px solid rgba(255, 255, 255, 0.8);
+        box-shadow: 
+            0px 4px 4px rgba(0, 0, 0, 0.25), 
+            0px 4px 4px rgba(0, 0, 0, 0.25), 
+            0px 4px 4px rgba(0, 0, 0, 0.25) inset;
+        color: #fff;
+        font-size: 16px;
+        height: 100px;
+        margin-top: 20px;
+        text-align: center;
+        width: 500px;
+    }
+</style>
